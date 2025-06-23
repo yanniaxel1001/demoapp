8016c3dd48814d0a5308b0a91b2ec24b5fef8adb
yanni <yanniaxelgalvanalatorre@gmail.com>
2025-06-23 14:22:03 -0600

    peticion ajax para buscar un elemento

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 
 class MainController extends AbstractController
@@ -13,6 +18,66 @@ class MainController extends AbstractController
     public function index(): Response
     {
         return $this->render('inicio/index.html.twig');
+    }
+
+    #[Route('/login', name:'login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // Obtener error login
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // Ultimo nombre de usuario ingresado
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/login.html.twig',[
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route('/registrar', name:'registrar', methods:['GET','POST'])]
+    public function registrar(
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher,
+        EntityManagerInterface $entityManager
+    ): Response{
+        if ($request->isMethod('POST')) 
+        {
+            $password = $request->request->get('password');
+            $confirmPassword = $request->request->get('confirm_password');
+            if ($password !== $confirmPassword) 
+            {
+                return $this->redirectToRoute('registrar');
+            }
+            // Procesar el formulario manualmente
+            $user = new User();
+            $user->setUsername($request->request->get('username'));
+            $user->setEmail($request->request->get('email'));
+            $user->setName($request->request->get('nombre'));
+            $user->setLastname($request->request->get('apellido'));
+            
+            // Hashear la contraseña
+            $user->setPassword(
+                    $passwordHasher->hashPassword(
+                    $user,
+                    $request->request->get('password')
+                )
+            );
+            
+            $user->setRoles(['ROLE_USER']);
+            
+            $entityManager->persist($user);
+            $entityManager->flush();
+            
+            return $this->redirectToRoute('login');
+        }
+        return $this->render('registro/registro.html.twig');
+    }
+
+    #[Route("/logout",name:"logout")]
+    public function logout(): void
+    {
+
     }
 }
 ?>
--- src/Controller/TableroController.php
+++ src/Controller/TableroController.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -14,19 +15,24 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class TableroController extends AbstractController
 {
     #[Route('/', name: 'product_index')]
-    public function index(EntityManagerInterface $em): Response
+    public function index(EntityManagerInterface $em, AuthenticationUtils $authenticationUtils): Response
     {
         $products = $em->getRepository(Product::class)->findBy([
             'owner' => $this->getUser()
         ]);
 
+        $lastUsername = $authenticationUtils->getLastUsername();
+
         return $this->render('dashboard/index.html.twig', [
-            'products' => $products
+            'products' => $products,
+            'last_username' => $lastUsername
         ]);
     }
 
-     #[Route('/create', name: 'product_create', methods: ['GET', 'POST'])]
-    public function create(Request $request, EntityManagerInterface $em): Response
+    #[Route('/create', name: 'product_create', methods: ['GET', 'POST'])]
+    public function create(
+        Request $request, 
+        EntityManagerInterface $em): Response
     {
         if ($request->isMethod('POST')) {
             $product = new Product();
@@ -44,11 +50,69 @@ class TableroController extends AbstractController
         return $this->render('dashboard/create.html.twig');
     }
 
-    #[Route("/logout",name:"logout")]
-    public function logout(): void
+    #[Route('/{id}/edit', name: 'product_edit', methods: ['GET','POST'])]
+    public function edit(
+        int $id,
+        Request $request,
+        EntityManagerInterface $em): Response
     {
+        $product = $em->getRepository(Product::class)->find($id);
 
+        if($request->isMethod('POST')){
+            $product->setName($request->request->get('name'));
+            $product->setDescription($request->request->get('description'));
+            $product->setPrice((float)$request->request->get('price'));
+            $em->flush();
+
+            return $this->redirectToRoute('product_index');
+        }
+
+        return $this->render('dashboard/edit.html.twig',[
+            'product'=>$product
+        ]);
     }
 
+    #[Route('/{id}/delete', name: 'product_delete', methods: ['POST'])]
+    public function delete(
+        int $id,
+        Request $request,
+        EntityManagerInterface $em): Response
+    {
+        $product = $em->getRepository(Product::class)->find($id);
+
+        $em->remove($product);
+        $em->flush();
+
+        return $this->redirectToRoute('product_index');
+    }
+    #[Route('/dashboard/buscar', name: 'product_search', methods: ['GET'])]
+    public function search(Request $request, 
+        EntityManagerInterface $em): JsonResponse
+    {
+        $termino = $request->query->get('q');
+        $usuario = $this->getUser();
+
+        $qb = $em->createQueryBuilder();
+        $qb->select('p')
+            ->from(Product::class, 'p')
+            ->where('p.owner = :user')
+            ->andWhere('p.name LIKE :term')
+            ->setParameter('user', $usuario)
+            ->setParameter('term', '%' . $termino . '%');
+
+        $productos = $qb->getQuery()->getResult();
+
+        $resultados = [];
+
+        foreach ($productos as $producto) {
+            $resultados[] = [
+                'id' => $producto->getId(),
+                'name' => $producto->getName(),
+                'price' => $producto->getPrice(),
+                'description' => $producto->getDescription()
+            ];
+        }
+        return new JsonResponse($resultados);
+    }
 }
 ?>
